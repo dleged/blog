6c99ef8f980196769d3c3ebeacf609b63c5ebf7b
faithZZZ <1208674494@qq.com>
2017-11-08 21:57:18 +0800

    修改:  动态分配当前列表页url，适合所有列表页翻页 添加: 列表删除功能

--- routers/admin.js
+++ routers/admin.js
@@ -37,41 +37,44 @@ router.get('/user',function(req,res,next){
         page = Math.max(page,1);     //page不能小于第1页
         skip = (page - 1) * limit;
         UserBase.find().limit(limit).skip(skip).then(function(users){
-            res.render('admin/user_index',{
+            res.render('admin/user_list',{
                 userInfo: req.userInfo,
                 responseData: {
                     code: 1,
                     message: '用户查询成功！',
                     data: users,
+                    /*分页组件参数*/
                     count: count,
                     pages: pages,
                     page: page,
-                    limit: limit
+                    limit: limit,
+                    url: '/admin/user'
                 }
             });
         });
     })
-
 })
 
 
 /*博客分类添页面*/
-router.get('/category',function(req,res,next){
+router.get('/category/add',function(req,res,next){
     responseData.code = 1;
-    res.render('admin/category',{
+    res.render('admin/category_add',{
         userInfo: req.userInfo,
         responseData: responseData
     });
 })
 
 /*博客分类保存*/
-router.post('/category',function(req,res,next){
+router.post('/category/add',function(req,res,next){
     var type = req.body.type;
     var mark = req.body.mark;
+    var author = req.userInfo.username;
+    console.log(author)
     if(!type || !mark){
         responseData.code = 0;
         responseData.message = '分类信息不能为空!';
-        return res.render('admin/category',{
+        return res.render('admin/category_add',{
             userInfo: req.userInfo,
             responseData: responseData
         });
@@ -86,19 +89,68 @@ router.post('/category',function(req,res,next){
                 responseData.message = '新增分类成功!';
                 var category = new CategoryBase({
                     type: type,
-                    mark: mark
+                    mark: mark,
+                    author: author
                 });
                 category.save();
             }
-            res.render('admin/category',{
+            res.render('admin/category_add',{
                 userInfo: req.userInfo,
                 responseData: responseData
             });
         });
     }
-
-
-
 });
+
+/*博客类型列表*/
+router.get('/category',function(req,res,next){
+    /*从用户数据库中读取用户数据*/
+    var limit = 5; //读取的条数
+    var page = Number(req.query.page || 1);//当前页数
+    var skip = 0; //（当前页-1）*limit 跳过的条数;
+    var pages = 0;//总条数
+    CategoryBase.count().then(function(count){
+        pages = Math.ceil(count/limit);
+        page = Math.min(page,pages); //page不能超过总页数
+        page = Math.max(page,1);     //page不能小于第1页
+        skip = (page - 1) * limit;
+        CategoryBase.find().limit(limit).skip(skip).then(function(users){
+            res.render('admin/category_list',{
+                userInfo: req.userInfo,
+                responseData: {
+                    code: 1,
+                    message: '用户查询成功！',
+                    data: users,
+                    /*分页组件参数*/
+                    count: count,
+                    pages: pages,
+                    page: page,
+                    limit: limit,
+                    url: '/admin/category'
+                }
+            });
+        });
+    })
+})
+
+/*博客类型删除*/
+router.get('/category/delete',function(req,res,next){
+    var type = req.query.type;
+    console.log(type);
+    CategoryBase.remove({type:type}).then(function(err){
+        if(!err){
+            res.render('admin/category_list',{
+                responseData: {
+                    code: 1,
+                    message: '删除成功！'
+                }
+            })
+
+        }
+    });
+})
+
+
+
 module.exports = router;
 
--- schemas/categorys.js
+++ schemas/categorys.js
@@ -6,7 +6,12 @@ var Schema = require('mongoose').Schema;
 var category = new Schema({
         type: String,
         mark: String,
+        author: String,
         createTime: {
+            type: Date,
+            default: Date.now
+        },
+        updateTime: {
             type: Date,
             default: Date.now
         }
