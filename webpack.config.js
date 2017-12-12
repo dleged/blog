b5702757535a8d6756298aedfa15a6d833ec7ab6
faithzzz <1208674494@qq.com>
2017-12-12 19:09:21 +0800

    webpack 工程化

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,5 @@ module.exports = {
     output: {
         path: path.resolve(__dirname,'dist'),//生产环境
         filename: 'app.bundle.js'
-    },
-    scripts: {
-        start: 'webpack',
-        dev: 'node dev-server.js'
     }
 }
