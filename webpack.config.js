6d9adf024223fbc7e83e5231e3ba6f8b9f12ad6d
faithzzz <1208674494@qq.com>
2017-12-14 19:26:41 +0700

    修改：webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,16 @@ module.exports = {
         path: path.resolve(__dirname,'dist'),//生产环境
         filename: 'app.bundle.js'
     },
-    module: {
-	    rules: [
-	      	{ test: /\.txt$/, use: 'raw-loader' }
-	    ]
-	}
+     module: {
+      	loaders: [{
+        	test: /\.css$/,
+        	loader: Ex.extract('style-loader', 'css-loader!postcss-loader') /*这里的写法注意下 */
+      	}]
+    },
+    postcss: function() {
+      	return [autoprefixer, cssnext, precss, cssnano]
+    },
+    plugins: [
+      	new Ex("【name】.css")
+    ]
 }
